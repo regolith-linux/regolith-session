5392edd35cf5d225e30be0245e785a015c1e2c57
Ken Gilmer <kgilmer@gmail.com>
2022-04-26 22:14:13 -0700

    Remove unnecessary Xres folder in /usr

--- usr/lib/regolith/regolith-session-common.sh
+++ usr/lib/regolith/regolith-session-common.sh
@@ -7,7 +7,6 @@ USER_XRESOURCE_SEARCH_PATH="$HOME/.config/regolith2/Xresources.d"
 
 # File Locations - System Defaults
 DEFAULT_XRESOURCE_LOOK_PATH="/usr/share/regolith-look/default"
-ETC_XRESOURCE_DIR="/usr/share/regolith/Xresources.d"
 
 DEFAULT_SYS_I3_CONFIG_FILE="/etc/regolith/i3/config"
 DEFAULT_USER_I3_CONFIG_FILE="$HOME/.config/regolith2/i3/config"
@@ -48,7 +47,6 @@ xres_i3_cleanup() {
 # Generate a Xresource file from merging the following into ~/.config/regolith2/Xresources-generated:
 # 1. /usr/share/regolith-look/default/root or override defined in ~/.Xresources
 # 2. ~/.config/regolith2/Xresources
-# 3. /usr/share/regolith/Xresources.d
 load_regolith_xres() {    
     GENERATED_XRES_DIR="$HOME/.cache/regolith2"
     GENERATED_XRES_FILE="$GENERATED_XRES_DIR/Xresources-generated"
@@ -65,13 +63,6 @@ load_regolith_xres() {
     if [ -f "$USER_XRESOURCE_OVERRIDE_FILE" ]; then
         printf "\n!+ Merged from %s at (%s)\n" "$USER_XRESOURCE_OVERRIDE_FILE" "$(date)" >> "$GENERATED_XRES_FILE"
         cat "$USER_XRESOURCE_OVERRIDE_FILE" >> "$GENERATED_XRES_FILE"
-    fi
-
-    if [ -d "$ETC_XRESOURCE_DIR" ]; then
-        for filename in "$ETC_XRESOURCE_DIR"/*; do
-            printf "\n!+ Merged from %s at (%s)\n" "$filename" "$(date)" >> "$GENERATED_XRES_FILE"
-            cat "$filename" >> "$GENERATED_XRES_FILE"
-        done
     fi
 
     xrdb -I"$USER_XRESOURCE_SEARCH_PATH" -merge "$GENERATED_XRES_FILE"
