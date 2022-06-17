a2d5de501fe622be967c7b95e7c3e5a30efe62c7
Ken Gilmer <kgilmer@gmail.com>
2022-06-17 04:47:07 -0700

    Add ability to stage looks in user directory ~/.config/regolith2/looks

--- usr/lib/regolith/regolith-session-common.sh
+++ usr/lib/regolith/regolith-session-common.sh
@@ -14,8 +14,9 @@ DEFAULT_USER_I3_CONFIG_FILE="$HOME/.config/regolith2/i3/config"
 # File Locations - Baseline
 BASELINE_XRESOURCE_FILE="$HOME/.Xresources"
 
-# Regolith Look base directory
+# Regolith Look directories
 DEFAULT_LOOK_ROOT="/usr/share/regolith-look"
+USER_LOOK_ROOT="$HOME/.config/regolith2/looks"
 
 # File location - user scripts
 USER_POST_LOGOUT_SCRIPT_FILE="$HOME/.config/regolith2/logout"
@@ -31,7 +32,7 @@ resolve_default_config_file() {
 }
 
 # Load default Xresources
-load_standard_xres() {    
+load_standard_xres() {
     if [ -f "$BASELINE_XRESOURCE_FILE" ]; then
         xrdb -merge "$BASELINE_XRESOURCE_FILE"
     fi
@@ -41,25 +42,25 @@ load_standard_xres() {
 # Due to a limitation of the preprocessor they have double quotes.
 # The i3-wm workspace command fails with double quotes in the name
 # NOTE: Calling this function in Regolith 2 desktop sessio was removed due to silent failures
-xres_i3_cleanup() {   
+xres_i3_cleanup() {
     xrdb -query |grep i3-wm.workspace.|sed "s/\"/'/g"|xrdb -merge
 }
 
 # 1. Load the Regolith Xresource override file if exists
 # 2. Load Regolith Look Xresource as defined in override or use default
 # 3. Re-merge Regolith Xresource override file if exists
-load_regolith_xres() {    
+load_regolith_xres() {
     if [ -f "$USER_XRESOURCE_OVERRIDE_FILE" ]; then
         xrdb -merge "$USER_XRESOURCE_OVERRIDE_FILE"
     fi
-    
+
     LOOK_STYLE_ROOT_PATH=$(xrescat regolith.look.path "$DEFAULT_XRESOURCE_LOOK_PATH")
     LOOK_STYLE_ROOT_FILE="$LOOK_STYLE_ROOT_PATH/root"
 
     if [ -f "$LOOK_STYLE_ROOT_FILE" ]; then
         xrdb -I"$USER_XRESOURCE_SEARCH_PATH" -merge "$LOOK_STYLE_ROOT_FILE"
-    fi    
-    
+    fi
+
     if [ -f "$USER_XRESOURCE_OVERRIDE_FILE" ]; then
         xrdb -I"$USER_XRESOURCE_SEARCH_PATH" -merge "$USER_XRESOURCE_OVERRIDE_FILE"
     fi
