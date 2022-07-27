aaf2c8022a35331ca5b1d1b1e30940d5bb012706
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-07-27 18:08:15 +0530

    Initial wayland setup

--- usr/lib/regolith/regolith-session-common.sh
+++ usr/lib/regolith/regolith-session-common.sh
@@ -11,6 +11,9 @@ DEFAULT_XRESOURCE_LOOK_PATH="/usr/share/regolith-look/default"
 DEFAULT_SYS_I3_CONFIG_FILE="/etc/regolith/i3/config"
 DEFAULT_USER_I3_CONFIG_FILE="$HOME/.config/regolith2/i3/config"
 
+DEFAULT_SYS_SWAY_CONFIG_FILE="/etc/regolith/sway/config"
+DEFAULT_USER_SWAY_CONFIG_FILE="$HOME/.config/regolith2/sway/config"
+
 # File Locations - Baseline
 BASELINE_XRESOURCE_FILE="$HOME/.Xresources"
 
@@ -21,13 +24,21 @@ USER_LOOK_ROOT="$HOME/.config/regolith2/looks"
 # File location - user scripts
 USER_POST_LOGOUT_SCRIPT_FILE="$HOME/.config/regolith2/logout"
 
-# Determine where the default i3 config file is
-# Sets I3_CONFIG_FILE
+DEFAULT_SESSION_TYPE="i3"
+
+# Determine where the default i3 and sway config files are
+# Sets I3_CONFIG_FILE and SWAY_CONFIG_FILE
 resolve_default_config_file() {
     if [ -f "$DEFAULT_USER_I3_CONFIG_FILE" ]; then
         I3_CONFIG_FILE="$DEFAULT_USER_I3_CONFIG_FILE"
     else
         I3_CONFIG_FILE="$DEFAULT_SYS_I3_CONFIG_FILE"
+    fi
+    
+    if [ -f "$DEFAULT_USER_I3_CONFIG_FILE" ]; then
+        SWAY_CONFIG_FILE="$DEFAULT_USER_I3_CONFIG_FILE"
+    else
+        SWAY_CONFIG_FILE="$DEFAULT_SYS_I3_CONFIG_FILE"
     fi
 }
 
@@ -35,6 +46,13 @@ resolve_default_config_file() {
 load_standard_xres() {
     if [ -f "$BASELINE_XRESOURCE_FILE" ]; then
         xrdb -merge "$BASELINE_XRESOURCE_FILE"
+    fi
+}
+
+# Load default Xresources
+load_standard_trawlres() {
+    if [ -f "$BASELINE_XRESOURCE_FILE" ]; then
+        trawldb --merge "$BASELINE_XRESOURCE_FILE"
     fi
 }
 
@@ -63,5 +81,24 @@ load_regolith_xres() {
 
     if [ -f "$USER_XRESOURCE_OVERRIDE_FILE" ]; then
         xrdb -I"$USER_XRESOURCE_SEARCH_PATH" -merge "$USER_XRESOURCE_OVERRIDE_FILE"
+    fi
+}
+
+# Loads the Xresource files into trawl. 
+# NOTE: Will load sway specific reource files
+load_regolith_trawlres() {
+    if [ -f "$USER_XRESOURCE_OVERRIDE_FILE" ]; then
+        trawldb --merge "$USER_XRESOURCE_OVERRIDE_FILE"
+    fi
+
+    LOOK_STYLE_ROOT_PATH=$(trawlcat regolith.look.path "$DEFAULT_XRESOURCE_LOOK_PATH")
+    LOOK_STYLE_ROOT_FILE="$LOOK_STYLE_ROOT_PATH/root"
+
+    if [ -f "$LOOK_STYLE_ROOT_FILE" ]; then
+        trawldb -I "$USER_XRESOURCE_SEARCH_PATH" --merge "$LOOK_STYLE_ROOT_FILE"
+    fi
+
+    if [ -f "$USER_XRESOURCE_OVERRIDE_FILE" ]; then
+        trawldb -I "$USER_XRESOURCE_SEARCH_PATH" --merge "$USER_XRESOURCE_OVERRIDE_FILE"
     fi
 }
