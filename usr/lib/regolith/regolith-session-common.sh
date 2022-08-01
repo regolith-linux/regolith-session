9e70528d799dd281530fba14b3799f92c957b3bd
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2022-08-02 02:57:21 +0530

    Overwrite cleaup file instead of appending to it

--- usr/lib/regolith/regolith-session-common.sh
+++ usr/lib/regolith/regolith-session-common.sh
@@ -72,7 +72,7 @@ xres_i3_cleanup() {
 }
 
 trawl_sway_cleanup() {
-    trawldb --query i3-wm.workspace.|sed "s/\"/'/g" >> $XDG_RUNTIME_DIR/trawl_sway_cleanup.tmp
+    trawldb --query i3-wm.workspace.|sed "s/\"/'/g" > $XDG_RUNTIME_DIR/trawl_sway_cleanup.tmp
     trawldb --merge $XDG_RUNTIME_DIR/trawl_sway_cleanup.tmp
 }
 
